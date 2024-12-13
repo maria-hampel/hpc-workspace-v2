c23f8fb512548e1381ffe6bc5d23163a028f5774
Holger Berger <holger.berger@googlemail.com>
2024-12-13 21:02:28 +0100

    add banner

--- src/caps.h
+++ src/caps.h
@@ -1,8 +1,35 @@
 #ifndef CAPABILITY_H
 #define CAPABILITY_H
 
+/*
+ *  hpc-workspace-v2
+ *
+ *  caps.h
+ * 
+ *  - privilege elevation with capabilites or setuid implementation
+ *
+ *  c++ version of workspace utility
+ *  a workspace is a temporary directory created in behalf of a user with a limited lifetime.
+ *
+ *  (c) Holger Berger 2021,2023,2024
+ * 
+ *  hpc-workspace-v2 is based on workspace by Holger Berger, Thomas Beisel and Martin Hecht
+ *
+ *  hpc-workspace-v2 is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  hpc-workspace-v2 is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with workspace-ng  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
-// TODO: add __LINE__ and __FILE__ again for ease of debugging?
 
 #include <sys/types.h>
 #include <unistd.h>
