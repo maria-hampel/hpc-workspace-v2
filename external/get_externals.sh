8936b36eb80cbb5b1206eb02229adae50a1a9c35
Holger Berger <holger.berger@googlemail.com>
2024-12-14 20:00:10 +0100

    add some intelligence

--- external/get_externals.sh
+++ external/get_externals.sh
@@ -1,5 +1,51 @@
-git clone https://github.com/catchorg/Catch2.git
-git clone https://github.com/fmtlib/fmt.git
-git clone https://github.com/jbeder/yaml-cpp.git
-git clone --recursive https://github.com/biojppm/rapidyaml.git
-git clone https://github.com/microsoft/GSL.git
+#!/bin/bash
+#
+# simple script to pull in dependencies or update them
+#
+OPWD=$PWD
+echo Catch2
+if [ ! -d Catch2 ]
+then
+	git clone https://github.com/catchorg/Catch2.git
+else 
+	cd Catch2
+	git pull
+	cd $OPWD
+fi
+echo fmt
+if [ ! -d fmt ]
+then
+	git clone https://github.com/fmtlib/fmt.git
+else 
+	cd Catch2
+	git pull
+	cd $OPWD
+fi
+echo yaml-cpp
+if [ ! -d yaml-cpp ]
+then
+	git clone https://github.com/jbeder/yaml-cpp.git
+else 
+	cd yaml-cpp
+	git pull
+	cd $OPWD
+fi
+echo rapidyaml
+if [ ! -d rapidyaml ]
+then
+	git clone --recursive https://github.com/biojppm/rapidyaml.git
+else 
+	cd rapidyaml
+	git pull 
+	git submodule update --recursive
+	cd $OPWD
+fi
+echo GSL
+if [ ! -d GSL ]
+then
+	git clone https://github.com/microsoft/GSL.git
+else 
+	cd GSL
+	git pull
+	cd $OPWD
+fi
