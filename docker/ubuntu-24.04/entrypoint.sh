e8b2530a0eedbe1eb7f2948cf27a2c9be07b4478
Holger Berger <holger.berger@googlemail.com>
2025-02-16 20:28:00 +0100

    bug fix for ws_restore test in container

--- docker/ubuntu-24.04/entrypoint.sh
+++ docker/ubuntu-24.04/entrypoint.sh
@@ -56,7 +56,8 @@ if [ "$run_bats_test" = true ] || [ "$run_ctest_test" = true ]; then
   cp build/debug/bin/ws_allocate /tmp/setuid
   cp build/debug/bin/ws_release /tmp/setuid
   cp build/debug/bin/ws_restore /tmp/setuid
-  cp build/debug/bin/ws_restore /tmp/setuid/ws_restore_notest
+  cp build/debug/bin/ws_restore build/debug/bin/ws_restore_notest
+  cp build/debug/bin/ws_restore_notest /tmp/setuid
   sudo chown root /tmp/setuid/ws_allocate
   sudo chmod u+s /tmp/setuid/ws_allocate
   sudo chmod u+s /tmp/setuid/ws_release
@@ -68,7 +69,8 @@ if [ "$run_bats_test" = true ] || [ "$run_ctest_test" = true ]; then
   cp build/release/bin/ws_allocate /tmp/cap
   cp build/release/bin/ws_release /tmp/cap
   cp build/release/bin/ws_restore /tmp/cap
-  cp build/release/bin/ws_restore /tmp/cap/ws_restore_notest
+  cp build/release/bin/ws_restore build/release/bin/ws_restore_notest
+  cp build/release/bin/ws_restore_notest /tmp/cap
   sudo setcap "CAP_DAC_OVERRIDE=p CAP_CHOWN=p CAP_FOWNER=p" /tmp/cap/ws_allocate
   sudo setcap "CAP_DAC_OVERRIDE=p CAP_CHOWN=p CAP_FOWNER=p" /tmp/cap/ws_release
   sudo setcap "CAP_DAC_OVERRIDE=p CAP_DAC_READ_SEARCH=p" /tmp/cap/ws_restore
