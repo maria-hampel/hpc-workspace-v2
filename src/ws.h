f71817c30638806de7e61f44eb90d04fb112f477
Holger Berger <holger.berger@googlemail.com>
2025-05-16 20:31:06 +0200

    add enum for extended ACL intent

---
+++ src/ws.h
@@ -0,0 +1,45 @@
+#ifndef WS_H
+#define WS_H
+/*
+ *  hpc-workspace-v2
+ *
+ *  ws.h
+ *
+ *  - global types and defines
+ *
+ *  c++ version of workspace utility
+ *  a workspace is a temporary directory created in behalf of a user with a limited lifetime.
+ *
+ *  (c) Holger Berger 2025
+ *
+ *  hpc-workspace-v2 is based on workspace by Holger Berger, Thomas Beisel and Martin Hecht
+ *
+ *  hpc-workspace-v2 is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  hpc-workspace-v2 is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with workspace-ng  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+ namespace ws {
+
+     // intent for ACLs
+     enum intent {
+        LIST,
+        USE,
+        CREATE,
+        EXTEND,
+        RELEASE,
+        RESTORE
+     }
+ }
+
+ #endif
