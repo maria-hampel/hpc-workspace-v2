b1a43b215975eb7620d884b909b79877a4253628
Holger Berger <holger.berger@googlemail.com>
2025-12-08 22:09:21 +0100

    accept @localhost as valid email address.

--- src/utils.cpp
+++ src/utils.cpp
@@ -233,13 +233,20 @@ void setCLocal() {
 bool isValidEmail(const std::string& email) {
     if (traceflag)
         spdlog::trace("isValidEmail({})", email);
+
     // Regular expression for basic email validation (improved)
     const std::regex email_regex(
         R"((?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\]))",
         std::regex_constants::icase); // Case-insensitive
 
+    const std::regex localemail_regex(
+        R"((?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@localhost)",
+        std::regex_constants::icase); // Case-insensitive
+
     if (!std::regex_match(email, email_regex)) {
-        return false;
+        if (!std::regex_match(email, localemail_regex)) {
+            return false;
+        }
     }
 
     // Additional checks (length limits, etc.)
