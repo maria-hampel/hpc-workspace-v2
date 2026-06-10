ed95ef061dca220074dad9d48094d2f88747197a
Maria Hampel <maria.hampel@mailbox.tu-dresden.de>
2026-06-10 10:48:58 +0200

    Improve Compare Script

--- onsite-test/cmp-all.sh
+++ onsite-test/cmp-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-ALL_FILES=$(ls onsite-test/workspace_logs | grep ws-expirer-scratch)
-for file in $ALL_FILES
-do
+for filepath in onsite-test/workspace_logs/ws-expirer-scratch-*.log; do
+  file=$(basename "$filepath")
   date=$(echo "$file" | grep -oE '[0-9]{4}-[0-9]{2}-[0-9]{2}')
-  python3 onsite-test/cmp-expirer-logs.py -o onsite-test/workspace_logs/$file -n onsite-test/workspace_logs/ws-expirer-v2-scratch-$date-01-00.log
+  python3 onsite-test/cmp-expirer-logs.py -o "$filepath" -n "onsite-test/workspace_logs/ws-expirer-v2-scratch-$date-01-00.log"
+  echo ""
 done
